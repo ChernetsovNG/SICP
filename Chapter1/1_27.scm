5a54dc766fad7b2f6c5dcd0f1f182f718a9a7b86
ChernetsovNG <n.chernetsov86@gmail.com>
2020-02-29 18:36:16 +0300

    Упражнение 1.27

---
+++ Chapter1/1_27.scm
@@ -0,0 +1,30 @@
+(define (carmichael n)
+  (carmichael-iter n 1))
+
+(define (carmichael-iter n a)
+  (cond ((and (< a n) (= (expmod a n n) a)) (carmichael-iter n (+ a 1)))
+	((= a n) "true")
+	(else "false")))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+	((divides? test-divisor n) test-divisor)
+	(else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+	((even? exp)
+	 (remainder (square (expmod base (/ exp 2) m))
+		    m))
+	(else
+	 (remainder (* base (expmod base (- exp 1) m))
+		    m))))
