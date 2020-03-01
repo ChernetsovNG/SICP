ca5d336c44d112e528a5ef04f0a680a984a17faa
ChernetsovNG <n.chernetsov86@gmail.com>
2020-03-01 18:11:00 +0300

    Программа для вычисления корня уравнения методом половинного деления

---
+++ Chapter1/programs-1_3_2.scm
@@ -0,0 +1,31 @@
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+	(b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+	   (search f a b))
+	  ((and (negative? b-value) (positive? a-value))
+	   (search f b a))
+	  (else
+	   (error "У аргументов не разные знаки " a b)))))
+
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+	midpoint
+	(let ((test-value (f midpoint)))
+	  (cond ((positive? test-value)
+		 (search f neg-point midpoint))
+		((negative? test-value)
+		 (search f midpoint pos-point))
+		(else midpoint))))))
+
+(define (average x y)
+  (/ (+ x y) 2.0))
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (abs x)
+  (cond ((< x 0) (- x))
+	((= x 0) 0)
+	((> x 0) x)))
